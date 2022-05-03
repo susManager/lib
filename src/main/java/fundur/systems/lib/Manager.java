02fd9cf17222b69c2d43b053c452e74e85d71911
LordBurtz <fridolin.karger@gmail.com>
2022-05-03 12:48:30 +0200

    done2

--- src/main/java/fundur/systems/lib/Manager.java
+++ src/main/java/fundur/systems/lib/Manager.java
@@ -1,8 +1,6 @@
 package fundur.systems.lib;
 
-import fundur.systems.lib.local.LocalManager;
 import fundur.systems.lib.sec.EncrState;
-import fundur.systems.lib.server.ServerManager;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -10,8 +8,8 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,18 +19,7 @@ import java.util.Scanner;
 import static fundur.systems.lib.sec.Security.*;
 
 public class Manager {
-    private final ExceptionHandler handler;
-    private boolean io, net;
-    private BaseManager manager;
-    private String path;
-
-    public Manager (ExceptionHandler handler) {
-        this.handler = handler;
-        this.path = getPath();
-        updateState();
-    }
-
-    public static String getPath() {
+    public static String getDefaultPath() {
         String sep = System.getProperty("file.separator");
         if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")) {
             return "%appdata%" + sep + "susManager" + sep;
@@ -41,55 +28,33 @@ public class Manager {
         }
     }
 
-    public void updateState() {
-        testIO();
-        testNet();
-        net = false; // due to server being implemented later on
-        manager = net ? new ServerManager() : new LocalManager();
-    }
-
-    public boolean testIO() {
-        io = true;
+    public static boolean testIO() {
         return true;
     }
 
-    public boolean testNet() {
+    public static boolean testNet() {
         try {
             URL url = new URL("https://example.org");
             HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            io = true;
             return true;
-        } catch (MalformedURLException ignored) {}
-          catch (IOException e) {
-            handler.handleException(e);
-        }
-        net = false;
+        } catch (Exception ignored) {}
         return false;
     }
 
-    public JSONObject getJSONObject(String hashUser, String password) throws Exception {
-        try {
-            String file = loadFile("config.json");
-            EncrState state = getState(new JSONObject(file).getJSONObject(hashUser));
-            String content = loadFile("testFile");
-            return new JSONObject(decrypt(state.algo(), content, getKeyFromPwd(password, state.salt()), new IvParameterSpec(state.iv())));
-        } catch (Exception e) {
-            handler.handleException(e);
-            throw e; //weil hier das error handling echt doppelt Sinn macht
-        }
+    public static JSONObject getJSONObject(String hashUser, String password, String path) throws Exception {
+        String file = loadFile(path + "config.json");
+        EncrState state = getState(new JSONObject(file).getJSONObject(hashUser));
+        String content = loadFile(path + hashUser);
+        return new JSONObject(decrypt(state.algo(), content, getKeyFromPwd(password, state.salt()), new IvParameterSpec(state.iv())));
     }
 
-    public boolean saveJSONObject (JSONObject jsonObject, String hashUser, String password) {
-        try {
-            String content = jsonObject.toString();
-            JSONObject config = new JSONObject(loadFile("config.json")).getJSONObject(hashUser);
-            EncrState state = getState(config);
-            String encrypted = encrypt(state.algo(), content, getKeyFromPwd(password, state.salt()), new IvParameterSpec(state.iv()));
-        } catch (Exception e) {
-            handler.handleException(e);
-        }
-        return false;
+    public static void saveJSONObject (JSONObject jsonObject, String hashUser, String password, String path) throws Exception {
+        String content = jsonObject.toString();
+        JSONObject config = new JSONObject(loadFile(path + "config.json")).getJSONObject(hashUser);
+        EncrState state = getState(config);
+        String encrypted = encrypt(state.algo(), content, getKeyFromPwd(password, state.salt()), new IvParameterSpec(state.iv()));
+        saveFile(encrypted, path + hashUser);
     }
 
     public static EncrState getState(JSONObject object) {
@@ -98,6 +63,12 @@ public class Manager {
             serer(object.getJSONArray("salt")),
             serer(object.getJSONArray("iv"))
         );
+    }
+
+    public static void saveFile(String content, String path) throws IOException {
+        FileWriter f = new FileWriter(path);
+        f.write(content);
+        f.flush();
     }
 
     public static String loadFile(String path) throws FileNotFoundException {
@@ -124,11 +95,26 @@ public class Manager {
 
     public static void main(String[] args) {
         try {
-            System.out.println(new Manager(null).getJSONObject(hash("fridolin"), "iHaveAids69"));
+            saveJSONObject(
+                new JSONObject("""
+                        { "passwords": {
+                            "gmail": {
+                                "name": "gmail",
+                                "usr": "cockUser@gmail.com",
+                                "pwd": "cockAndBall",
+                                "timestamp": 1651574125604
+                            }
+                          }
+                        }
+                        """),
+                hash("fridolin"),
+                "iHaveAids69",
+                ""
+            );
+
+            System.out.println(getJSONObject(hash("fridolin"), "iHaveAids69", ""));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.println();
     }
 }
